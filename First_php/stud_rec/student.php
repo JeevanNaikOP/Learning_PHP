f9a826a925142c725edbe16414f50921f5622a73
JeevanNaikOP <61685578+JeevanNaikOP@users.noreply.github.com>
2020-06-08 15:35:50 +0530

    Create student.php

---
+++ First_php/stud_rec/student.php
@@ -0,0 +1,15 @@
+<html>
+<body>
+
+<h1>student info</h1>
+<form action="get.php" method="get">
+<b>Name<b> <input type="text" name="stud_name">
+<b>age<b> <input type="text" name="stud_age">
+<b>weight<b> <input type="text" name="stud_weight">
+<br>
+<input type="submit">
+
+
+</form>
+</body>
+</html>
