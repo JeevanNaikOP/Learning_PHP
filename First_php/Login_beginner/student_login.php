b5c64484c3ceb62ebf536cde387763b963532a55
JeevanNaikOP <61685578+JeevanNaikOP@users.noreply.github.com>
2020-06-09 12:15:31 +0530

    Create student_login.php

---
+++ First_php/Login_beginner/student_login.php
@@ -0,0 +1,15 @@
+<html>
+<body>
+
+<h1>student info</h1>
+<form action="post.php" method="post">
+<b>userame<b> <input type="text" name="username">
+<b>password<b> <input type="password" name="password">
+
+<br>
+<input type="submit">
+
+
+</form>
+</body>
+</html>
